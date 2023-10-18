87f06cade77f6e31b8a48a5edcea6fabdb5e3876
Yoann <lb.yoann16@gmail.com>
2023-10-18 05:09:56 +0200

    Test menu-principal

--- src/component/recup-page/PopGenrePage2.js
+++ src/component/recup-page/PopGenrePage2.js
@@ -10,21 +10,34 @@ const PopGenrePage = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=List_of_music_genres_and_styles&prop=text&origin=*'
+          'https://fr.wikipedia.org/w/api.php?action=parse&format=json&page=Genre_musical&prop=text&origin=*'
         );
 
         if (response.data && response.data.parse && response.data.parse.text) {
           const pageContent = response.data.parse.text['*'];
           const $popContent = $("<div>").html(pageContent); // Convertir le texte en éléments jQuery
+
+          //convertir les balise en a en p
+         //$popContent.find("a").replaceWith(function() { return $("<div>").addClass("circle").append($("<p>").append($(this).contents())); });
+
+          $popContent.find("td").find("div:last").remove(); // supprimer le dernier div
+
+
+          // supprimer les nodetype 3
+          $popContent.find("td").contents().filter(function() {
+            return this.nodeType === 3;
+          }
+          ).remove();
           
           // Utiliser jQuery pour extraire le contenu entre les balises h2 "Histoire" et la prochaine balise h2
-          const extractedContent = $popContent.find("div").nextUntil("div").clone();
+          //const extractedContent = $popContent.find("div").nextUntil("div").clone();
+          const extractedContent = $popContent.find("td:first").clone();
 
           // Récupérer les balises h2 "Histoire" également
           const h2Elements = $popContent.find("h2").clone();
 
           // Mettre à jour le contenu d'affichage
-          setPopContent([...h2Elements.toArray(), ...extractedContent.toArray()]);
+          setPopContent([ ...extractedContent.toArray()]);
         }
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -33,6 +46,8 @@ const PopGenrePage = () => {
 
     fetchData();
   }, []);
+
+
 
   useEffect(() => {
     if (popContent.length > 0 && popContentRef.current) {
@@ -48,7 +63,7 @@ const PopGenrePage = () => {
   return (
     <div>
       <h1>Pop Music Wikipedia Info</h1>
-      <div className="pop-content" ref={popContentRef} />
+      <div className="pop-content" ref={popContentRef}/>
       <style>
         {`
           /* Ajoutez vos styles CSS personnalisés ici */
@@ -58,6 +73,16 @@ const PopGenrePage = () => {
             border: 1px solid #ccc;
             background-color: #f7f7f7;
             color: #333;
+          }
+          .circle {
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            background: #ccc;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 10px;
           }
         `}
       </style>
